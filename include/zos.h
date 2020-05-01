fae2d6a7731428885df7aeeb661f42a8e42f9a18
Igor Todorovski <itodorov@ca.ibm.com>
2020-05-01 09:36:10 -0400

    Add z/OS control blocks

--- include/zos.h
+++ include/zos.h
@@ -24,6 +24,8 @@ typedef unsigned long size_t;
 #define MINOR_VERSION 1
 #define PATCH_VERSION 0
 
+struct timespec;
+
 typedef enum {
   CLOCK_REALTIME,
   CLOCK_MONOTONIC,
@@ -42,7 +44,48 @@ typedef struct __sem {
 typedef struct Semaphore {
   ____sem_t *_s;
 } __sem_t;
-struct timespec;
+
+// Contains z/OS Control Block Structures for retreiving system information.
+// CPU Management Control Table (CCT).
+typedef struct ZOSCCT {
+  uint8_t filler [110];
+  uint16_t cpuCount;  // Number of online CPUs.
+} ZOSCCT_t;
+
+
+// System Resources Manager Control Table (RMCT).
+typedef struct ZOSRMCT {
+  uint8_t name[4];
+  ZOSCCT* __ptr32 cct;
+} ZOSRMCT_t;
+
+
+// RSM Control and Enumeration Area (RCE).
+typedef struct ZOSRCE {
+  uint8_t id[4];
+  uint32_t pool;  // Number of frames currently available to system.
+} ZOSRCE_t;
+
+
+// Communications Vector Table (CVT).
+typedef struct ZOSCVT {
+  uint8_t filler[604];
+  ZOSRMCT* __ptr32 rmct;
+  uint8_t filler1[560];
+  ZOSRCE* __ptr32 rce;
+  uint8_t filler2[92];
+  uint8_t cvtoslvl[16];
+} ZOSCVT_t;
+
+
+// Prefixed Save Area (PSA).
+// Maps storage starting at location 0.
+typedef struct ZOSPSA {
+  uint8_t filler[16];  // Ignore 16 bytes before CVT pointer.
+  ZOSCVT* __ptr32 cvt;
+} ZOSPSA_t;
+
+// zoslib interfaces
 extern void* _convert_e2a(void* dst, const void* src, size_t size);
 extern void* _convert_a2e(void* dst, const void* src, size_t size);
 int clock_gettime(clockid_t clk_id, struct timespec* tp);
