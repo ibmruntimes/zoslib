f49e0dcb8bc9face677dd3b027d3621d00ce6226
Shuowang (Wayne) Zhang <shuowang.zhang@ibm.com>
2020-11-23 18:06:47 -0500

    test: add __e2a_l(), __a2e_l(), and clean up tests

--- test/test-convert.cc
+++ test/test-convert.cc
@@ -1,6 +1,8 @@
 #include "zos.h"
 #include "gtest/gtest.h"
 
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
 namespace {
 
 static const char *ascii[] = {"A",
@@ -19,8 +21,8 @@ static const char *ebcdic[] = {"A",
 #pragma convert(pop)
 
 TEST(E2ATest, ConvertE2A) {
-  for (int i = 0; i < sizeof(ebcdic) / sizeof(ebcdic[0]); i++) {
-    const size_t len = strlen(ascii[i]) + 1;
+  for (int i = 0; i < ARRAY_SIZE(ebcdic); i++) {
+    const size_t len = strlen(ebcdic[i]) + 1;
     char *const buffer = new char[len];
     _convert_e2a(buffer, ebcdic[i], len);
     EXPECT_STREQ(ascii[i], buffer);
@@ -28,11 +30,33 @@ TEST(E2ATest, ConvertE2A) {
   }
 }
 
-TEST(A2ETest, ConvertA2E) {
-  for (int i = 0; i < sizeof(ascii) / sizeof(ascii[0]); i++) {
+TEST(E2ATest, E2AL) {
+  for (int i = 0; i < ARRAY_SIZE(ebcdic); i++) {
     const size_t len = strlen(ebcdic[i]) + 1;
     char *const buffer = new char[len];
+    strncpy(buffer, ebcdic[i], len);
+    __e2a_l(buffer, len);
+    EXPECT_STREQ(ascii[i], buffer);
+    delete[] buffer;
+  }
+}
+
+TEST(A2ETest, ConvertA2E) {
+  for (int i = 0; i < ARRAY_SIZE(ascii); i++) {
+    const size_t len = strlen(ascii[i]) + 1;
+    char *const buffer = new char[len];
     _convert_a2e(buffer, ascii[i], len);
+    EXPECT_STREQ(ebcdic[i], buffer);
+    delete[] buffer;
+  }
+}
+
+TEST(A2ETest, A2EL) {
+  for (int i = 0; i < ARRAY_SIZE(ascii); i++) {
+    const size_t len = strlen(ascii[i]) + 1;
+    char *const buffer = new char[len];
+    strncpy(buffer, ascii[i], len);
+    __a2e_l(buffer, len);
     EXPECT_STREQ(ebcdic[i], buffer);
     delete[] buffer;
   }
