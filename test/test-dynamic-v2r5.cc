2b6cbba0cf661bfdc092fa5f9e17f05189b4461d
Igor Todorovski <itodorov@ca.ibm.com>
2021-10-05 12:16:04 -0400

    add tests for v2r5 dynamic le vector funtions

---
+++ test/test-dynamic-v2r5.cc
@@ -0,0 +1,54 @@
+#include "zos.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(DynamicV2R5, clock_gettime) {
+  struct timespec start, stop;
+  double accum;
+  EXPECT_EQ(clock_gettime( CLOCK_REALTIME, &start), 0);
+  system("sleep 1");
+  EXPECT_EQ(clock_gettime( CLOCK_REALTIME, &stop), 0);
+  EXPECT_GT(stop.tv_sec, start.tv_sec);
+  EXPECT_GT(stop.tv_nsec, start.tv_nsec);
+}
+
+class DynamicV2R5Temp : public ::testing::Test {
+    virtual void SetUp() {
+      temp_path = tmpnam(NULL);
+      temp_fp = fopen(temp_path, "w");
+    }
+
+    virtual void TearDown() {
+      fclose(temp_fp);
+      remove(temp_path);
+    }
+
+protected:
+    char* temp_path;
+    FILE *temp_fp;
+};
+
+
+TEST_F(DynamicV2R5Temp, lutimes) {
+  struct timeval t[]= { { 0, 0 } , { 0, 0 } };
+  EXPECT_EQ(lutimes(temp_path, t), 0);
+  struct stat buff;
+  lstat(temp_path, &buff);
+  EXPECT_EQ(buff.st_atime, 0);
+  EXPECT_EQ(buff.st_mtime, 0);
+}
+
+TEST_F(DynamicV2R5Temp, futimes) {
+  if (!__is_os_level_at_or_above(ZOSLVL_V2R5)) 
+    GTEST_SKIP();
+
+  struct timeval t[]= { { 0, 0 } , { 0, 0 } };
+  EXPECT_EQ(futimes(fileno(temp_fp), t), 0);
+  struct stat buff;
+  fstat(fileno(temp_fp), &buff);
+  EXPECT_EQ(buff.st_atime, 0);
+  EXPECT_EQ(buff.st_mtime, 0);
+}
+
+} // namespace
