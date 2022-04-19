ibmruntimes/zoslib

commit 8d757f2321148559266eb96ce5d22976c8058192

replace use of alloca() (#72)

src/zos-char-util.cc
@@ -260,29 +260,29 @@ int __guess_fd_ue(int fd, char *errmsg, size_t er_size, int is_new_fd) {
     return -1;
   }
 
-  char *buffer;
-  int ccsid;
-
   // Only guess first CCSID_GUESS_BUF_SIZE_ENVAR byte of data at most
   if (ccsid_guess_buf_size <= 4096) {
-    buffer = (char *)alloca(ccsid_guess_buf_size);
+    char buffer[ccsid_guess_buf_size];
+    ssize_t bytes = read(fd, buffer, ccsid_guess_buf_size);
+    if (bytes < 0) {
+      perror("guess_ue:read");
+      return -1;
+    }
+    buffer[bytes] = '\0';
+    return __guess_ue(buffer, (size_t)bytes, errmsg, er_size);
   } else {
-    buffer = (char *)malloc(ccsid_guess_buf_size);
-  }
-  ssize_t bytes = read(fd, buffer, ccsid_guess_buf_size);
-  if (bytes < 0) {
-    perror("guess_ue:read");
-    ccsid = -1;
-    goto quit;
-  }
-  buffer[bytes] = '\0';
-
-  ccsid = __guess_ue(buffer, (size_t)bytes, errmsg, er_size);
-quit:
-  if (ccsid_guess_buf_size > 4096)
+    char *buffer = (char *)malloc(ccsid_guess_buf_size);
+    ssize_t bytes = read(fd, buffer, ccsid_guess_buf_size);
+    if (bytes < 0) {
+      perror("guess_ue:read");
+      free(buffer);
+      return -1;
+    }
+    buffer[bytes] = '\0';
+    int ccsid = __guess_ue(buffer, (size_t)bytes, errmsg, er_size);
     free(buffer);
-
-  return ccsid;
+    return ccsid;
+  }
 }
 
 int __guess_ae(const void *src, size_t size) {
@@ -520,22 +520,21 @@ int __file_needs_conversion_init(const char *name, int fd) {
       unsigned len = strlen_ae((unsigned char *)buf, &ccsid, cnt, &am);
       if (ccsid == 1047 && len == cnt) {
         if (no_tag_read_behaviour == __NO_TAG_READ_DEFAULT_WITHWARNING) {
-          const char *filename = "(null)";
           if (name) {
-            int len = strlen(name);
-            filename =
-                (const char *)_convert_e2a(alloca(len + 1), name, len + 1);
+            len = strlen(name) + 1;
+            char filename[len];
+            _convert_e2a(filename, name, len);
+            dprintf(2, "Warning: File \"%s\" is untagged and seems to contain "
+                       "EBCDIC characters\n", filename);
           }
-          dprintf(2,
-                  "Warning: File \"%s\" is untagged and seems to "
-                  "contain EBCDIC "
-                  "characters\n",
-                  filename);
+        } else {
+          dprintf(2, "Warning: File (null) is untagged and seems to contain "
+                     "EBCDIC characters\n");
         }
         fdcache.set_attribute(fd, 0x0000000000020000UL);
         return 1;
       }
-    }       // seekable files
+    }       // cnt > 8
   }         // seekable files
   return 0; // not seekable
 }

include/zos-char-util.h
@@ -144,7 +144,7 @@ void __fd_close(int fd);
   ({                                                                           \
     const char *src = (const char *)(_str);                                    \
     int len = strlen(src) + 1;                                                 \
-    char *tgt = (char *)alloca(len);                                           \
+    char tgt[len];                                                             \
     (char *)_convert_e2a(tgt, src, len);                                       \
   })
 
@@ -152,7 +152,7 @@ void __fd_close(int fd);
   ({                                                                           \
     const char *src = (const char *)(_str);                                    \
     int len = strlen(src) + 1;                                                 \
-    char *tgt = (char *)alloca(len);                                           \
+    char tgt[len];                                                             \
     (char *)_convert_a2e(tgt, src, len);                                       \
   })
 

src/zos-io.cc
@@ -67,7 +67,6 @@ void __console(const void *p_in, int len_i) {
 
 int __console_printf(const char *fmt, ...) {
   va_list ap;
-  char *buf;
   int len;
   va_start(ap, fmt);
   va_list ap1;
@@ -82,22 +81,25 @@ int __console_printf(const char *fmt, ...) {
   if (ccsid == 819) {
     mode = __ae_thread_swapmode(__AE_ASCII_MODE);
     bytes = __vsnprintf_a(0, 0, fmt, ap1);
-    buf = (char *)alloca(bytes + 1);
+    char buf[bytes + 1];
     len = __vsnprintf_a(buf, bytes + 1, fmt, ap2);
     __a2e_l(buf, len);
+    va_end(ap2);
+    va_end(ap1);
+    va_end(ap);
+    if (len > 0)
+      __console(buf, len);
   } else {
     mode = __ae_thread_swapmode(__AE_EBCDIC_MODE);
     bytes = __vsnprintf_e(0, 0, fmt, ap1);
-    buf = (char *)alloca(bytes + 1);
+    char buf[bytes + 1];
     len = __vsnprintf_e(buf, bytes + 1, fmt, ap2);
+    va_end(ap2);
+    va_end(ap1);
+    va_end(ap);
+    if (len > 0)
+      __console(buf, len);
   }
-  va_end(ap2);
-  va_end(ap1);
-  va_end(ap);
-  if (len <= 0)
-    goto quit;
-  __console(buf, len);
-quit:
   __ae_thread_swapmode(mode);
   return len;
 }
@@ -109,7 +111,6 @@ int vdprintf(int fd, const char *fmt, va_list ap) {
   int mode;
   int len;
   int bytes;
-  char *buf;
   va_list ap1;
   va_list ap2;
   va_copy(ap1, ap);
@@ -117,25 +118,24 @@ int vdprintf(int fd, const char *fmt, va_list ap) {
   if (ccsid == 819) {
     mode = __ae_thread_swapmode(__AE_ASCII_MODE);
     bytes = __vsnprintf_a(0, 0, fmt, ap1);
-    buf = (char *)alloca(bytes + 1);
+    char buf[bytes + 1];
     len = __vsnprintf_a(buf, bytes + 1, fmt, ap2);
+    if (len > 0)
+      len = write(fd, buf, len);
   } else {
     mode = __ae_thread_swapmode(__AE_EBCDIC_MODE);
     bytes = __vsnprintf_e(0, 0, fmt, ap1);
-    buf = (char *)alloca(bytes + 1);
+    char buf[bytes + 1];
     len = __vsnprintf_e(buf, bytes + 1, fmt, ap2);
+    if (len > 0)
+      len = write(fd, buf, len);
   }
-  if (len == -1)
-    goto quit;
-  len = write(fd, buf, len);
-quit:
   __ae_thread_swapmode(mode);
   return len;
 }
 
 int dprintf(int fd, const char *fmt, ...) {
   va_list ap;
-  char *buf;
   int len;
   va_start(ap, fmt);
   va_list ap1;
@@ -150,21 +150,24 @@ int dprintf(int fd, const char *fmt, ...) {
   if (ccsid == 819) {
     mode = __ae_thread_swapmode(__AE_ASCII_MODE);
     bytes = __vsnprintf_a(0, 0, fmt, ap1);
-    buf = (char *)alloca(bytes + 1);
+    char buf[bytes + 1];
     len = __vsnprintf_a(buf, bytes + 1, fmt, ap2);
+    va_end(ap2);
+    va_end(ap1);
+    va_end(ap);
+    if (len > 0)
+      len = write(fd, buf, len);
   } else {
     mode = __ae_thread_swapmode(__AE_EBCDIC_MODE);
     bytes = __vsnprintf_e(0, 0, fmt, ap1);
-    buf = (char *)alloca(bytes + 1);
+    char buf[bytes + 1];
     len = __vsnprintf_e(buf, bytes + 1, fmt, ap2);
+    va_end(ap2);
+    va_end(ap1);
+    va_end(ap);
+    if (len > 0)
+      len = write(fd, buf, len);
   }
-  va_end(ap2);
-  va_end(ap1);
-  va_end(ap);
-  if (len == -1)
-    goto quit;
-  len = write(fd, buf, len);
-quit:
   __ae_thread_swapmode(mode);
   return len;
 }

src/zos.cc
@@ -145,11 +145,10 @@ extern "C" void __xfer_env(void) {
   int i;
   int len;
   char *str;
-  char *a_str;
   while (*start) {
     str = *start;
     len = strlen(str);
-    a_str = (char *)alloca(len + 1);
+    char a_str[len + 1];
     memcpy(a_str, str, len);
     a_str[len] = 0;
     for (i = 0; i < len; ++i) {
@@ -466,8 +465,10 @@ int strcasecmp_ignorecp(const char *a, const char *b) {
     return len_a - len_b;
   if (!memcmp(a, b, len_a))
     return 0;
-  char *a_new = (char *)_convert_e2a(alloca(len_a + 1), a, len_a + 1);
-  char *b_new = (char *)_convert_e2a(alloca(len_b + 1), b, len_b + 1);
+  char a_new[len_a + 1];
+  _convert_e2a(a_new, a, len_a + 1);
+  char b_new[len_b + 1];
+  _convert_e2a(b_new, b, len_b + 1);
   __convert_one_to_one(ascii_to_lower, a_new, len_a, a_new);
   __convert_one_to_one(ascii_to_lower, b_new, len_b, a_new);
   return strcmp(a_new, b_new);
@@ -478,30 +479,25 @@ int strncasecmp_ignorecp(const char *a, const char *b, size_t n) {
   int am_a, am_b;
   unsigned len_a = strlen_ae((unsigned char *)a, &ccsid_a, n, &am_a);
   unsigned len_b = strlen_ae((unsigned char *)b, &ccsid_b, n, &am_b);
-  char *a_new;
-  char *b_new;
   if (len_a != len_b)
     return len_a - len_b;
-
+  char a_new[len_a + 1];
+  char b_new[len_b + 1];
   if (ccsid_a != 819) {
-    a_new = (char *)__convert_one_to_one(__ibm1047_iso88591, alloca(len_a + 1),
-                                         len_a, a);
+    __convert_one_to_one(__ibm1047_iso88591, a_new, len_a, a);
     a_new[len_a] = 0;
-    a_new = (char *)__convert_one_to_one(ascii_to_lower, a_new, len_a, a_new);
+    __convert_one_to_one(ascii_to_lower, a_new, len_a, a_new);
   } else {
-    a_new = (char *)__convert_one_to_one(ascii_to_lower, alloca(len_a + 1),
-                                         len_a, a);
+    __convert_one_to_one(ascii_to_lower, a_new, len_a, a);
     a_new[len_a] = 0;
   }
 
   if (ccsid_b != 819) {
-    b_new = (char *)__convert_one_to_one(__ibm1047_iso88591, alloca(len_b + 1),
-                                         len_b, b);
+    __convert_one_to_one(__ibm1047_iso88591, b_new, len_b, b);
     b_new[len_b] = 0;
-    b_new = (char *)__convert_one_to_one(ascii_to_lower, b_new, len_b, b_new);
+    __convert_one_to_one(ascii_to_lower, b_new, len_b, b_new);
   } else {
-    b_new = (char *)__convert_one_to_one(ascii_to_lower, alloca(len_b + 1),
-                                         len_b, b);
+    __convert_one_to_one(ascii_to_lower, b_new, len_b, b);
     b_new[len_b] = 0;
   }
 
@@ -639,9 +635,10 @@ extern "C" int __dlcb_entry_name(char *buf, int size, void *dlcb) {
     return 0;
   n = ((unsigned short *)dlcb)[44];
   name = ((char **)dlcb)[12];
+  char tmpbuf[n + 1];
   return __snprintf_a(
       buf, size, "%-.*s", n,
-      __convert_one_to_one(__ibm1047_iso88591, alloca(n + 1), n, name));
+      __convert_one_to_one(__ibm1047_iso88591, tmpbuf, n, name));
 }
 extern "C" void *__dlcb_entry_addr(void *dlcb) {
   if (dlcb == 0)
@@ -1560,32 +1557,32 @@ extern "C" int anon_munmap(void *addr, size_t len) {
 
 extern "C" int execvpe(const char *name, char *const argv[],
                        char *const envp[]) {
-  int lp, ln;
-  const char *p;
-
-  int eacces = 0, etxtbsy = 0;
-  char *bp, *cur, *path, *buf = 0;
-
   // Absolute or Relative Path Name
   if (strchr(name, '/')) {
     return execve(name, argv, envp);
   }
+  int lp, ln;
+  const char *p;
+  int eacces = 0, etxtbsy = 0;
+  char *bp, *cur;
 
   // Get the path we're searching
-  if (!(path = getenv("PATH"))) {
-    if ((cur = path = (char *)alloca(2)) != NULL) {
-      path[0] = ':';
-      path[1] = '\0';
-    }
-  } else {
-    char *n = (char *)alloca(strlen(path) + 1);
-    strcpy(n, path);
-    cur = path = n;
+  int len;
+  if (cur = getenv("PATH"))
+    len = strlen(cur);
+  else
+    len = 1;
+  char path[len + 1];
+  if (cur)
+    strcpy(path, cur);
+  else {
+    path[0] = ':';
+    path[1] = '\0';
+    cur = path;
   }
 
-  if (path == NULL ||
-      (bp = buf = (char *)alloca(strlen(path) + strlen(name) + 2)) == NULL)
-    goto done;
+  char buf[len + strlen(name) + 2];
+  bp = buf;
 
   while (cur != NULL) {
     p = cur;
@@ -1619,13 +1616,13 @@ extern "C" int execvpe(const char *name, char *const argv[],
 
       for (cnt = 0, ap = (char **)argv; *ap; ++ap, ++cnt)
         ;
-      if ((ap = (char **)alloca((cnt + 2) * sizeof(char *))) != NULL) {
-        memcpy(ap + 2, argv + 1, cnt * sizeof(char *));
+      char tbuf[(cnt + 2) * sizeof(char *)];
+      ap = (char **)tbuf;
+      memcpy(ap + 2, argv + 1, cnt * sizeof(char *));
 
-        ap[0] = (char *)"sh";
-        ap[1] = bp;
-        (void)execve("/bin/sh", ap, envp);
-      }
+      ap[0] = (char *)"sh";
+      ap[1] = bp;
+      (void)execve("/bin/sh", ap, envp);
       goto done;
     }
     case ETXTBSY:

commit 2491674e68115da77605c5ba6b1690f7222e821e

char-util: fix regression causing file tag warning (#73)
When an untagged file is opened, a warning is now displayed
even if env variable __UNTAGGED_READ_MODE is not set (to WARN).
The regression was caused by commit 8d757f23.

src/zos-char-util.cc
@@ -526,10 +526,10 @@ int __file_needs_conversion_init(const char *name, int fd) {
             _convert_e2a(filename, name, len);
             dprintf(2, "Warning: File \"%s\" is untagged and seems to contain "
                        "EBCDIC characters\n", filename);
+          } else {
+            dprintf(2, "Warning: File (null) is untagged and seems to contain "
+                       "EBCDIC characters\n");
           }
-        } else {
-          dprintf(2, "Warning: File (null) is untagged and seems to contain "
-                     "EBCDIC characters\n");
         }
         fdcache.set_attribute(fd, 0x0000000000020000UL);
         return 1;