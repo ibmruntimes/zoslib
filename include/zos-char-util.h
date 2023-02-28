136a7d88fdae0ac452c98f6c4784f456e9cc0b38
Igor Todorovski <itodorov@ca.ibm.com>
2023-02-28 14:47:33 -0500

    Fix runtime issue with vla

--- include/zos-char-util.h
+++ include/zos-char-util.h
@@ -156,7 +156,7 @@ __Z_EXPORT void __fd_close(int fd);
   ({                                                                           \
     const char *src = (const char *)(_str);                                    \
     int len = strlen(src) + 1;                                                 \
-    char tgt[len];                                                             \
+    char* tgt = (char*)alloca(len);                                             \
     (char *)_convert_a2e(tgt, src, len);                                       \
   })
 
