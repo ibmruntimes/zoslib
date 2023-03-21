67eae658d8a077ebac65773740a8a2dcb68020f2
Igor Todorovski <itodorov@ca.ibm.com>
2023-03-21 15:49:05 -0400

    Add an ascii version of mkfifo

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -9,9 +9,47 @@
 #ifndef ZOS_SYS_STAT_H
 #define ZOS_SYS_STAT_H
 
+#include "zos-macros.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+/**
+ * Same as C open but tags new files as ASCII (819)
+ */
+__Z_EXPORT extern int __mkfifo_ascii(const char *pathname, mode_t mode);
+
+#if defined(__cplusplus)
+};
+#endif
+
+#if defined(ZOSLIB_OVERRIDE_CLIB)
+
+#undef mkfifo
+#define mkfifo __mkfifo_replaced
+#include_next <sys/stat.h>
+#undef mkfifo 
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+__Z_EXPORT extern int mkfifo(const char *pathname, mode_t mode) asm("__mkfifo_ascii");
+
+#if defined(__cplusplus)
+};
+#endif
+
+#else // #if !(defined(ZOSLIB_OVERRIDE_CLIB)
+
 #include_next <sys/stat.h>
 
-#ifndef S_TYPEISMQ  
+#endif
+
+#ifdef S_TYPEISMQ  
+#undef S_TYPEISMQ
+#undef S_TYPEISSEM
+#undef S_TYPEISSHM 
 #define S_TYPEISMQ(__x)   (0) /* Test for a message queue */
 #define S_TYPEISSEM(__x)  (0) /* Test for a semaphore     */
 #define S_TYPEISSHM(__x)  (0) /* Test for a shared memory */
