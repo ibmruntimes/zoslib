7a8ee1fabd94822e618314853cda991b6574dbbf
Igor Todorovski <itodorov@ca.ibm.com>
2023-03-29 16:14:48 -0400

    Add missing zoslib-help.cc

---
+++ src/zoslib-help.cc
@@ -0,0 +1,8 @@
+#include "zos-base.h"
+#include <stdio.h>
+
+int main()
+{
+  __print_zoslib_help(stdout, "ZOSLIB Environment Variables\n"
+                              "----------------------------");
+}
