4b40bdcbfec25611551d7360012324451a7693db
Shuowang (Wayne) Zhang <shuowang.zhang@ibm.com>
2023-06-21 18:00:39 -0400

    test: fix getentropy() buffer is not null check
    There was a bug with getentropy() that caused the buffer to be null.
    Previously, a test was introduced which checked to ensure that the first
    element in the buffer was not 0. However, since the buffer is filled
    with random values, 0 is a valid value for the first element. Instead,
    the entire buffer should be checked to make sure that at least one
    element is non-zero.
    
    PR-URL: https://github.ibm.com/open-z/zoslib/pull/104

--- test/test-getentropy.cc
+++ test/test-getentropy.cc
@@ -1,20 +1,40 @@
-#include "zos-getentropy.h"
+#include <unistd.h>
 #include "gtest/gtest.h"
 
 namespace {
 
 TEST(GetentropyTest, Getentropy256) {
   char buf[256];
+  memset(buf, 0, sizeof(buf));
+
   int rc = getentropy(buf, sizeof(buf));
   EXPECT_EQ(rc, 0);
-  EXPECT_NE(((char*)buf)[0], 0);
+
+  bool buffer_is_null = true;
+  for (int i = 0; i < sizeof(buf); i++) {
+    if (buf[i] != 0) {
+      buffer_is_null = false;
+      break;
+    }
+  }
+  EXPECT_FALSE(buffer_is_null);
 }
 
 TEST(GetentropyTest, Getentropy257) {
   char buf[257];
+  memset(buf, 0, sizeof(buf));
+
   int rc = getentropy(buf, sizeof(buf));
   EXPECT_EQ(rc, 0);
-  EXPECT_NE(((char*)buf)[0], 0);
+
+  bool buffer_is_null = true;
+  for (int i = 0; i < sizeof(buf); i++) {
+    if (buf[i] != 0) {
+      buffer_is_null = false;
+      break;
+    }
+  }
+  EXPECT_FALSE(buffer_is_null);
 }
 
 TEST(GetentropyTest, Getentropy258) {
