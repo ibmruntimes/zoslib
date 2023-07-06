9876dfe274fa3ea34fb34133ed99f34766f154d7
Igor Todorovski <itodorov@ca.ibm.com>
2023-07-06 14:23:53 -0400

    Add sys/sysmacros.h + chdir_long

---
+++ include/sys/sysmacros.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+// Licensed Materials - Property of IBM
+// ZOSLIB
+// (C) Copyright IBM Corp. 2021. All Rights Reserved.
+// US Government Users Restricted Rights - Use, duplication
+// or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef ZOS_SYSMACROS_H_
+#define ZOS_SYSMACROS_H_
+
+#define major(x)    (((unsigned)(x) >> 8) & 0x7f)
+#define minor(x)    ((x) & 0xff)
+#define makedev(maj, min) (unsigned short)(((x) << 8) | ((y) & 0xff))
+
+#endif
