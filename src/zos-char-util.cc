ibmruntimes/zoslib

commit 5e5deca6d2235b962d19314e5c73f97915636170

Fix __set_autocvt_on_fd_stream so that it doesn't set the tag on the file, plus remove _TAG_REDIR envars

src/zos-char-util.cc
@@ -543,18 +543,10 @@ int __file_needs_conversion_init(const char *name, int fd) {
 
 void __set_autocvt_on_fd_stream(int fd, unsigned short ccsid,
                                 unsigned char txtflag, int on_untagged_only) {
-  struct file_tag tag;
-
-  tag.ft_ccsid = ccsid;
-  tag.ft_txtflag = txtflag;
-  tag.ft_deferred = 0;
-  tag.ft_rsvflags = 0;
-
   struct f_cnvrt req = {SETCVTON, 0, (short)ccsid};
 
-  if (!on_untagged_only || (!isatty(fd) && 0 == __getfdccsid(fd))) {
+  if (!on_untagged_only || 0 == __getfdccsid(fd)) {
     fcntl(fd, F_CONTROL_CVT, &req);
-    fcntl(fd, F_SETTAG, &tag);
   }
 }
 

src/zos.cc
@@ -2730,18 +2730,6 @@ int __zinit::initialize(const zoslib_config_t &aconfig) {
     setenv("_BPXK_AUTOCVT", "ON", 1);
   }
 
-  tenv = getenv("_TAG_REDIR_IN");
-  if (!tenv || !*tenv)
-    setenv("_TAG_REDIR_IN", "txt", 1);
-
-  tenv = getenv("_TAG_REDIR_OUT");
-  if (!tenv || !*tenv)
-    setenv("_TAG_REDIR_OUT", "txt", 1);
-
-  /* Causes double conversion when spawning sh */
-  tenv = getenv("_TAG_REDIR_ERR");
-  if (tenv)
-    unsetenv("_TAG_REDIR_ERR");
 
   std::string ccsid;
   if (get_env_var("__STDIN_CCSID", ccsid))

commit bd40a0c8fc03f777d3efc524a1aee6cd70bf1795

revert rm of F_SETTAG in_set_autocvt_on_fd_stream (TEMP)
This reverts part of 5e5deca6d, which caused files created
from redirected stderr|out to be untagged. This is temporary
as discussed with Igor, till a solution to guard against
double conversion is found.

src/zos-char-util.cc
@@ -559,10 +559,18 @@ int __file_needs_conversion_init(const char *name, int fd) {
 
 void __set_autocvt_on_fd_stream(int fd, unsigned short ccsid,
                                 unsigned char txtflag, int on_untagged_only) {
+  struct file_tag tag;
+
+  tag.ft_ccsid = ccsid;
+  tag.ft_txtflag = txtflag;
+  tag.ft_deferred = 0;
+  tag.ft_rsvflags = 0;
+
   struct f_cnvrt req = {SETCVTON, 0, (short)ccsid};
 
-  if (!on_untagged_only || 0 == __getfdccsid(fd)) {
+  if (!on_untagged_only || (!isatty(fd) && 0 == __getfdccsid(fd))) {
     fcntl(fd, F_CONTROL_CVT, &req);
+    fcntl(fd, F_SETTAG, &tag);
   }
 }
 