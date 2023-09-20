ddde202621322f3faef343f40cac496221896243
Igor Todorovski <itodorov@ca.ibm.com>
2023-09-20 15:16:14 -0400

    fixup

--- include/zos-v2r5-symbolfixes.h
+++ include/zos-v2r5-symbolfixes.h
@@ -1,7 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Licensed Materials - Property of IBM
 // ZOSLIB
-// (C) Copyright IBM Corp. 2022. All Rights Reserved.
+// (C) Copyright IBM Corp. 2023. All Rights Reserved.
 // US Government Users Restricted Rights - Use, duplication
 // or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
 ///////////////////////////////////////////////////////////////////////////////
