69bf5f023cfac6f601846270f5bd59c603fc7874
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-11-22 07:47:49 -0500

    Update zos-v2r5-symbolfixes.h (#34)
    Add shm_open to the list

--- include/zos-v2r5-symbolfixes.h
+++ include/zos-v2r5-symbolfixes.h
@@ -45,6 +45,7 @@
 #pragma redefine_extname setresgid setresgid_undefined
 #pragma redefine_extname dup3 dup3_undefined
 #pragma redefine_extname flock flock_undefined
+#pragma redefine_extname shm_open shm_open_undefined
 #endif
 
 #endif // ZOS_V2R5_FIXUPS_H
