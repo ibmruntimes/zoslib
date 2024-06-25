11eeb205e1205dcfcc05e201613fe3ee87dfb181
Igor Todorovski <itodorov@ca.ibm.com>
2024-06-25 10:52:15 -0400

    Add missing header netiniet

---
+++ include/netinet/in_systm.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+// Licensed Materials - Property of IBM
+// ZOSLIB
+// (C) Copyright IBM Corp. 2024. All Rights Reserved.
+// US Government Users Restricted Rights - Use, duplication
+// or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef ZOS_IN_SYSTM_H_
+#define ZOS_IN_SYSTM_H_
+
+#include <stdint.h>
+
+// Network types
+typedef uint16_t n_short; 
+typedef uint32_t n_long;
+typedef uint32_t n_time;
+
+#endif 
