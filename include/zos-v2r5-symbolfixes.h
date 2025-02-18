c8defc58aea4f66964837885b139081b187448f8
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2025-02-17 22:58:25 -0500

    Update zos-v2r5-symbolfixes.h
    Add remaining v2r5 symbols

--- include/zos-v2r5-symbolfixes.h
+++ include/zos-v2r5-symbolfixes.h
@@ -8,9 +8,8 @@
 #ifndef ZOS_V2R5_SYMBOLFIXES_H
 #define ZOS_V2R5_SYMBOLFIXES_H
 
-// This enables builds on >=V2R5 systems when the target is <V2R5
-// Redefines >=V2R5 symbols with _undefined suffix to trigger a linker 
-// error so they are not detected by configure scripts
+// Redefines V2R5+ symbols with _undefined suffix to trigger a linker 
+// error so they are not detected by autotools-based configure scripts. 
 #if (__TARGET_LIB__ < 0x42050000)
 #pragma redefine_extname readlinkat readlinkat_undefined
 #pragma redefine_extname openat openat_undefined
@@ -51,6 +50,44 @@
 #pragma redefine_extname eventfd eventfd_undefined
 #pragma redefine_extname wait4 wait4_undefined
 #pragma redefine_extname madvise madvise_undefined
+
+// Additional v2r5 functions based on analysis in https://github.com/orgs/zopencommunity/discussions/855#discussioncomment-12192678
+#pragma redefine_extname accept4 accept4_undefined
+#pragma redefine_extname clone clone_undefined
+#pragma redefine_extname epoll_create epoll_create_undefined
+#pragma redefine_extname epoll_create1 epoll_create1_undefined
+#pragma redefine_extname epoll_ctl epoll_ctl_undefined
+#pragma redefine_extname epoll_pwait epoll_pwait_undefined
+#pragma redefine_extname epoll_wait epoll_wait_undefined
+#pragma redefine_extname fdclosedir fdclosedir_undefined
+//#pragma redefine_extname flock flock_undefined // Defined in zoslib
+#pragma redefine_extname fremovexattr fremovexattr_undefined
+#pragma redefine_extname fsetxattr fsetxattr_undefined
+//#pragma redefine_extname getentropy getentropy_undefined // Defined in zoslib
+#pragma redefine_extname getopt_long getopt_long_undefined
+#pragma redefine_extname getpwent_r getpwent_r_undefined
+#pragma redefine_extname inet_aton inet_aton_undefined
+#pragma redefine_extname inotify_add_watch inotify_add_watch_undefined
+#pragma redefine_extname inotify_init1 inotify_init1_undefined
+#pragma redefine_extname inotify_rm_watch inotify_rm_watch_undefined
+#pragma redefine_extname lremovexattr lremovexattr_undefined
+#pragma redefine_extname lsetxattr lsetxattr_undefined
+#pragma redefine_extname pivot_root pivot_root_undefined
+#pragma redefine_extname removexattr removexattr_undefined
+#pragma redefine_extname setns setns_undefined
+#pragma redefine_extname setxattr setxattr_undefined
+#pragma redefine_extname statfs statfs_undefined
+#pragma redefine_extname umount2 umount2_undefined
+#pragma redefine_extname unshare unshare_undefined
+//#pragma redefine_extname vasprintf vasprintf_undefined // Defined in zoslib
+#pragma redefine_extname openat2 openat2_undefined
+#pragma redefine_extname syscall syscall_undefined
+#pragma redefine_extname fgetxattr fgetxattr_undefined
+#pragma redefine_extname flistxattr flistxattr_undefined
+#pragma redefine_extname getxattr getxattr_undefined
+#pragma redefine_extname lgetxattr lgetxattr_undefined
+#pragma redefine_extname listxattr listxattr_undefined
+#pragma redefine_extname llistxattr llistxattr_undefined
 #endif
 
 #endif // ZOS_V2R5_SYMBOLFIXES_H
