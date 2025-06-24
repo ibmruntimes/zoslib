528cf51fde1cc9c1175a656265ff2984ace97560
Aryan Patel <124106308+apatel896@users.noreply.github.com>
2025-06-23 22:44:54 -0400

    document statfs override

--- include/sys/statfs.h
+++ include/sys/statfs.h
@@ -12,7 +12,7 @@
 #include "zos-macros.h"
 #include <features.h>
 
-
+// struct statfs in zoslib's sys/mount.h should be used and not the one in LE's sys/statfs.h
 #if defined(ZOSLIB_OVERRIDE_CLIB) || defined(ZOSLIB_OVERRIDE_CLIB_STATFS)
 /* Modify function names in header to avoid conflict with new prototypes */
 #undef statfs
