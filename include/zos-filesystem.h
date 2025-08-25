5619ae8cdd85f6407c651005dc402e2a88d0bf04
Sachin T <sachintu47@gmail.com>
2025-08-25 10:16:26 -0400

    Add cpp guard for filesystem header

--- include/zos-filesystem.h
+++ include/zos-filesystem.h
@@ -1,9 +1,10 @@
 #ifndef ZOS_FILESYSTEM_H
 #define ZOS_FILESYSTEM_H
 
+#ifdef __cplusplus
 #include <string.h>
 #include <cstdint>
 
 std::uintmax_t portable_remove_all(const std::string& path);
-
+#endif
 #endif
